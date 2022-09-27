5b4befebb09264e0ecf54b8d807e748e39173a7d
thfcmenow <47439369+thfcmenow@users.noreply.github.com>
2022-09-26 20:45:33 -0400

    Update server.js

--- server.js
+++ server.js
@@ -6,27 +6,64 @@ const app = express()
 const path = require('path');
 const port = process.env.PORT || 3000;
 
-const config = require("./json/config.json");
+process.env.CYCLIC_DB = 'zany-gold-armadillo-bootCyclicDB'
+const CyclicDb = require("cyclic-dynamodb")
+const db = CyclicDb("zany-gold-armadillo-bootCyclicDB")
+
+// const config = require("./json/config.json");
 
 //setting view engine to ejs
 app.set("view engine", "ejs");
 
 var retData = ""
 
+// retrieve data
+const run = async function(){
+  let pets = db.collection('Pets')
+  let work = await pets.item('Yuki').fragment('visits').get()
+  retData = JSON.stringify(work)
+ /* let item = await pets.get('Yuki')
+  console.log(item)
+  retData = JSON.stringify(item)
+  console.log(retData)
+  console.log(item.props.Dates)
+  console.log(item.props.Dates[0].complete)
+  let yuki = await pets.set(item.props.Dates[0].complete,"true")*/
+}
+
+run()
+
+
 // index
 app.get('/', function (req, res) {
    res.render("index", {
-    data: config
+    data: retData
   });
 });
 
 // update
 router.get("/update", (request, response) => {
   let json = request.query.main
+
+  let updateDB = async function(){
+  let pets = db.collection('Pets')
+  await pets.item('Yuki')
+        .fragment('visits').set({
+            dates: request.query.main
+        })
+      }
+  /*
+  delete json['updated'];
+  delete json['created']
   let serverdata = JSON.stringify(json);
-  response.write(serverdata)
-  response.send()
-  fs.writeFileSync('./json/config.json', serverdata);
+  console.log(serverdata)
+  let updateDB = async function(){
+    let pets = db.collection('Pets')
+    // let yuki = await pets.set('Yuki', serverdata)
+    let yuki = await pets.set('Yuki',JSON.stringify(json))
+    pets.set()
+  } */
+  updateDB()
 })
 
 app.get('/yuki.JPG', function (req, res) {
