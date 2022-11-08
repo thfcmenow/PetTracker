b753000522871048d184b034ca4cc12f83c270f7
thfcmenow <47439369+thfcmenow@users.noreply.github.com>
2022-11-08 08:53:37 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -6,31 +6,12 @@ const app = express()
 const path = require('path');
 const port = process.env.PORT || 3000;
 
-/*process.env.CYCLIC_DB = 'zany-gold-armadillo-bootCyclicDB'
-const CyclicDb = require("cyclic-dynamodb")
-const db = CyclicDb("zany-gold-armadillo-bootCyclicDB")*/
+
 
 const config = require("./json/config.json");
 
 //setting view engine to ejs
 app.set("view engine", "ejs");
-
-var retData = ""
-
-// retrieve data
-const run = async function(){
- /* let pets = db.collection('Pets')
-  let work = await pets.item('Yuki').fragment('visits').get()
-  retData = JSON.stringify(work)*/
- //let item = await pets.get('Yuki')
-
-  //retData = JSON.stringify(item)
- 
-  //let yuki = await pets.set(item.props.Dates[0].complete,"true")*/
-}
-
-//run()
-
 
 // index
 app.get('/', function (req, res) {
@@ -39,30 +20,30 @@ app.get('/', function (req, res) {
   });
 });
 
-// update
-router.get("/update", (request, response) => {
-  let json = request.query.main
+// change ignored dates
+app.get('/admin/:dates', function (req, res) {
+  let dates = req.params.dates
 
-  let updateDB = async function(){
-  let pets = db.collection('Pets')
-  await pets.item('Yuki')
-        .fragment('visits').set({
-            dates: request.query.main
-        })
-      }
-  /*
-  delete json['updated'];
-  delete json['created']
-  let serverdata = JSON.stringify(json);
-  console.log(serverdata)
-  let updateDB = async function(){
-    let pets = db.collection('Pets')
-    // let yuki = await pets.set('Yuki', serverdata)
-    let yuki = await pets.set('Yuki',JSON.stringify(json))
-    pets.set()
-  } */
-  updateDB()
+  if (dates == "read"){
+  fs.readFile('ignoredDates.txt', 'utf8', function(err, data){
+    res.send(data);
 })
+  } else if (dates == "blank") {
+
+    
+  
+      fs.writeFile("ignoredDates.txt", dates, 'utf8', function (err) {
+         
+    });
+    res.send("OK")
+      } else {
+  
+  fs.writeFile("ignoredDates.txt", dates, 'utf8', function (err) {
+    
+});
+res.send("OK")
+  }
+});
 
 app.get('/yuki.JPG', function (req, res) {
   res.sendFile(path.join(__dirname, '/yuki.JPG'))
@@ -72,6 +53,6 @@ app.use("/", router)
 
 app.listen(port);
 
-console.log('Server started at on port: ' + port);
+
 
 
