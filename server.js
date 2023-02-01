ea6187470d4bd3574d52aef3bc64e14a9e5982eb
David Russell <thfcme@gmail.com>
2023-01-31 22:22:51 -0500

    removed junk, added silly image effect

--- server.js
+++ server.js
@@ -6,45 +6,19 @@ const app = express()
 const path = require('path');
 const port = process.env.PORT || 3000;
 
-
-
 const config = require("./json/config.json");
 
 //setting view engine to ejs
 app.set("view engine", "ejs");
 
-// index
+// index router
 app.get('/', function (req, res) {
    res.render("index", {
     data: config
   });
 });
 
-// change ignored dates
-app.get('/admin/:dates', function (req, res) {
-  let dates = req.params.dates
-
-  if (dates == "read"){
-  fs.readFile('ignoredDates.txt', 'utf8', function(err, data){
-    res.send(data);
-})
-  } else if (dates == "blank") {
-
-    
-  
-      fs.writeFile("ignoredDates.txt", dates, 'utf8', function (err) {
-         
-    });
-    res.send("OK")
-      } else {
-  
-  fs.writeFile("ignoredDates.txt", dates, 'utf8', function (err) {
-    
-});
-res.send("OK")
-  }
-});
-
+// image of the great Yuki and co
 app.get('/yuki.JPG', function (req, res) {
   res.sendFile(path.join(__dirname, '/yuki.JPG'))
 })
