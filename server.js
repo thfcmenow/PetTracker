0b7a141ef31d90d959812f72de9d2b26edff8acb
David Russell <thfcme@gmail.com>
2023-02-01 20:35:50 -0500

    changed to get call for json

--- server.js
+++ server.js
@@ -1,32 +1,27 @@
-// require('dotenv').config()
-const fs = require('fs');
 const express = require("express")
 const router = express.Router()
 const app = express()
-const path = require('path');
 const port = process.env.PORT || 3000;
 
-const config = require("./json/config.json");
+// const config = require("./json/config.json"); // if use for scriptlets
+const config = {}
 
 //setting view engine to ejs
 app.set("view engine", "ejs");
 
-// index router
+// index
 app.get('/', function (req, res) {
    res.render("index", {
     data: config
   });
 });
 
-// image of the great Yuki and co
-app.get('/yuki.JPG', function (req, res) {
-  res.sendFile(path.join(__dirname, '/yuki.JPG'))
-})
+// JSON
+app.use('/json', express.static('json'))
+
+// CSS, images, public resources
+app.use('/public', express.static('public'))
 
 app.use("/", router)
 
 app.listen(port);
-
-
-
-
