581d95d3860cdd54380b6f7a51de83251d1d700e
David Russell <thfcme@gmail.com>
2023-06-03 10:24:31 -0400

    fix init and added route for specific records

--- server.js
+++ server.js
@@ -3,6 +3,28 @@ const router = express.Router()
 const app = express()
 const port = process.env.PORT || 3000;
 
+const CyclicDb = require("@cyclic.sh/dynamodb")
+const db = CyclicDb(process.env.DB_NAME)
+const visits = db.collection(process.env.DB_COLLECTION)
+
+// dynamo section ---------------------------
+// retreive specific record number from dynamo
+async function getDynamo(recordNum) {
+  let item = await visits.get(recordNum);
+  return item;
+}
+
+async function visualizeDynamo(res,recordNum) {
+  let result = await getDynamo(recordNum);
+  res.json(result.props.data)
+}
+
+app.get('/getDetails', function(req, res){
+  if (req.query.name.length == 2) visualizeDynamo(res,req.query.name)
+})
+// end dynamo -----------------------------
+
+
 // const config = require("./json/config.json"); // if use for scriptlets
 const config = {}
 
